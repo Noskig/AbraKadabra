c6a5dbb5f9c9c07d86629b1d25e1665fd7fed754
Noskig <48559023+Noskig@users.noreply.github.com>
2022-02-21 11:08:03 +0100

    Built and code showing

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-// get values
+// Get values
 function getValues(){
 
     let first = parseInt(document.getElementById("firstNumber").value);
@@ -6,29 +6,34 @@ function getValues(){
 
     if(Number.isInteger(first) && Number.isInteger(second)){
 
-        let allValues = generateValues(frist, second)
+        //call Generate
+        let allValues = generateValues(first, second)
+
+        //call display
+        displayValues(allValues);
+
     }
     else{
         alert("You have to enter numbers")
     }
 }
 
-// generate all the numbers to 100
+// Generate all the numbers to 100
 function generateValues(first, second){
 
     let returnValues = [];
 
-    for (let index = 0; index <= 100; index++) {
+    for (let index = 1; index <= 100; index++) {
         
         if(index % first == 0 && index % second == 0){
 
-            returnValues.push("n1n2");
+            returnValues.push("AbraKadabra");
         }
         else if(index % second == 0){
-            returnValues.push("n2");
+            returnValues.push("Kadabra");
         }
         else if(index % first == 0){
-            returnValues.push("n1");
+            returnValues.push("Abra");
         }
         else{
             returnValues.push(index);
@@ -38,4 +43,93 @@ function generateValues(first, second){
     return returnValues;
 }
 
-//dispaly
+function generateValuesb(first,second){
+
+    let returnArray = [];
+
+    let fixx = false;
+    let buzz = false;
+
+    for (let i = 1; i < 100; i++) {
+        
+        fizz = i % first == 0;
+        buzz = i % second == 0;
+
+        switch(true){
+
+            case fizz && buzz:{
+                returnArray.push('FizzBuzz');
+                break;
+            }
+            case fizz:{
+                returnArray.push('fizz');
+                break;
+            }
+            case buzz:{
+                returnArray.push('buzz');
+                break;
+            }
+            default:{
+                returnArray.push(i);
+            }
+
+
+        }
+        
+    }
+
+
+
+    return returnArray;
+}
+
+function generateValuesc(first, second){
+
+    let returnArray = [];
+
+    for (let i = 1; i < 100; i++) {
+        
+        let value = ( (i % first == 0 ? 'Fizz' : '' ) + (i % second == 0 ? 'buzz' : '' || i) );
+        returnArray.push(value);
+    }
+    return returnArray
+}
+// Display
+function displayValues(allValues){
+
+    //get the table body element 
+    let tablebody = document.getElementById("results");
+    
+    //get the template row
+    let templateRow = document.getElementById("fbTemplate");
+
+    //clear table
+    tablebody.innerHTML = "";
+    
+    for(let index = 0; index < allValues.length; index+=5){
+
+        let tableRow = document.importNode(templateRow.content, true);
+
+        //grab td's into an array
+        let rowCols = tableRow.querySelectorAll("td"); 
+
+        //add content to the class. Either Abra, Kadabra, AbraKadabra or a number. 
+        rowCols[0].classList.add(allValues[index]);
+        rowCols[0].textContent = allValues[index];
+
+        rowCols[1].classList.add(allValues[index+1]);
+        rowCols[1].textContent = allValues[index+1];
+
+        rowCols[2].classList.add(allValues[index+2]);
+        rowCols[2].textContent = allValues[index+2];
+
+        rowCols[3].classList.add(allValues[index+3]);
+        rowCols[3].textContent = allValues[index+3];
+
+        rowCols[4].classList.add(allValues[index+4]);
+        rowCols[4].textContent = allValues[index+4];
+
+        tablebody.appendChild(tableRow);
+
+    }
+}
